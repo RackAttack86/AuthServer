944936854df9837f41aac2aebda6a512555a7ba4
Matthew Rackley <rackley86@gmail.com>
2026-02-20 22:40:15 -0500

    Created post mapping in health controller to manually test AuthClientService.

--- src/main/java/com/rackleet/authserver/controller/HealthController.java
+++ src/main/java/com/rackleet/authserver/controller/HealthController.java
@@ -1,16 +1,29 @@
 package com.rackleet.authserver.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rackleet.authserver.entity.OAuthClient;
 import com.rackleet.authserver.exception.OAuthError;
 import com.rackleet.authserver.exception.OAuthException;
+import com.rackleet.authserver.service.ClientAuthenticationService; 
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
+@RequiredArgsConstructor
 public class HealthController {
+
+    private final ClientAuthenticationService clientAuthenticationService;
 
     @GetMapping("/health")
     public Map<String, String> health() {
@@ -24,4 +37,15 @@ public class HealthController {
                 "This is a test error",
                 HttpStatus.UNAUTHORIZED);
     }
+
+    // Temporary for testing. TODO: Remove this method after completed testing.
+    @PostMapping("/test-client-auth")
+    public Map<String, String> testClientAuth(HttpServletRequest request) {
+        OAuthClient client = clientAuthenticationService.authenticateClient(request);
+        return Map.of(
+            "authenticated_client", client.getClientId(),
+            "auth_method", client.getTokenEndpointAuthMethod()
+        );
+    }
+    
 }
