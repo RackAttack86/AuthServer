397cb3b730d54c4a603d9eb20749d4d8749df2b1
Matthew Rackley <rackley86@gmail.com>
2026-02-24 23:53:11 -0500

    Created UserRegistrationRequest

---
+++ src/main/java/com/rackleet/authserver/dto/request/UserRegistrationRequest.java
@@ -0,0 +1,22 @@
+package com.rackleet.authserver.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegistrationRequest {
+
+    @NotBlank(message = "username is required")
+    @Size(min = 3, max = 100, message = "username must be between 3 and 100 characters")
+    private String username;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "password must be at least 8 characters")
+    private String password;
+}
